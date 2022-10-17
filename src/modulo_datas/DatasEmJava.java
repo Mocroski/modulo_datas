ffafe0f4ffbc281fd29b575b286ecd282ad41c92
Lucas Mocroski <devmocroski@gmail.com>
2022-10-17 15:40:57 -0300

    "Faixa de tempo com ChronoUnit"

--- src/modulo_datas/DatasEmJava.java
+++ src/modulo_datas/DatasEmJava.java
@@ -2,6 +2,8 @@ package modulo_datas;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -9,15 +11,20 @@ public class DatasEmJava {
 
     public static void main(String[] args) throws ParseException {
 
-      Calendar calendar = Calendar.getInstance();/*usa data atual*/
+     /* Calendar calendar = Calendar.getInstance();*//*usa data atual*//*
 
-      /*simular que a data vem do bacnoc de dados*/
+      *//*simular que a data vem do bacnoc de dados*//*
 
-      calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("10-03-2022"));/*definindo data qualquer*/
+      calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("10-03-2022"));*//*definindo data qualquer*//*
 
       calendar.add(Calendar.DAY_OF_MONTH, 5);
 
-        System.out.println(new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+        System.out.println(new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));*/
+
+
+
+        long dias = ChronoUnit.DAYS.between(LocalDate.parse("2022-02-01"), LocalDate.now());
+        System.out.println("Possui " + dias + " entrre a faixa de data");
 
     }
 }
